6a3a74a51d7dd3b5fa81778d5906b6670904785a
Madushan97 <madushannipuna1997@gmail.com>
2023-08-12 20:40:32 +0530

    allow html, css, Js to render without authentication

---
+++ src/main/java/com/madushan/Spring/Security/student/Student.java
@@ -0,0 +1,14 @@
+package com.madushan.Spring.Security.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student {
+
+    private Integer studentId;
+    private String studentName;
+}
---
+++ src/main/java/com/madushan/Spring/Security/student/StudentController.java
@@ -0,0 +1,30 @@
+package com.madushan.Spring.Security.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+        new Student(1, "Madushan"),
+        new Student(2, "Nipuna"),
+        new Student(3, "Kamal"),
+        new Student(4, "Amila")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Enter valid ID"));
+    }
+}
